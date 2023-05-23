aaada501fba7aea9e74bd070accf816e46e923f2
stevegachau.eth <steve@zawadi.co>
2023-05-23 18:43:25 +0300

    Update index.js

--- API/avatar/index.js
+++ API/avatar/index.js
@@ -1,29 +1,8 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloHttp', async (req, res) => {
- 
-  res.set('Access-Control-Allow-Origin', '*');
-
-  if (req.method === 'OPTIONS') {
-    // Send response to OPTIONS requests
-    res.set('Access-Control-Allow-Methods', '*');
-    res.set('Access-Control-Allow-Headers', '*');
-    res.status(204).send('');
-  } 
-  
-  else{
- 
   var id = parseInt(req.query.id, 10);
   const resp = await fetch("https://api.opensea.io/v2/metadata/optimism/"+req.query.nft+"/"+id);
-  if (resp.ok) {
   const data = await resp.json();
-  const image = data.image;
-  console.log(image);
-  res.redirect(image);
-  }
-  else{
-  res.send();
-  }
-
-  }
+  res.redirect(data.image);
 });
