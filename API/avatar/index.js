c658bb6c4ef29a5c415d7dbaf3ab46b59b087c98
stevegachau.eth <steve@zawadi.co>
2024-01-31 16:59:36 +0300

    Update avatar chain

--- API/avatar/index.js
+++ API/avatar/index.js
@@ -1,8 +1,9 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloHttp', async (req, res) => {
+  var chain = "optimism" //optimism or base
   var id = parseInt(req.query.id, 10);
-  const resp = await fetch("https://api.opensea.io/v2/metadata/optimism/"+req.query.nft+"/"+id);
+  const resp = await fetch("https://api.opensea.io/v2/metadata/chain/"+req.query.nft+"/"+id); 
   const data = await resp.json();
   res.redirect(data.image);
 });
