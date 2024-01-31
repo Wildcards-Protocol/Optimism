10b2f07b2b0c378a14ae1c18cbe981198204080b
stevegachau.eth <steve@zawadi.co>
2024-02-01 00:11:19 +0300

    Update index.js

--- API/ccip-forwarder-gateway/index.js
+++ API/ccip-forwarder-gateway/index.js
@@ -6,8 +6,8 @@
  */
 exports.helloWorld = (req, res) => {
     const Web3 = require('web3');
-    const web3 = new Web3("...........");  //Optimism http rpc url
-    const address = ".........."; //Optimism_Wildcard_Registry Contract address
+    const web3 = new Web3("...........");  //L2 http rpc url
+    const address = ".........."; //Wildcard_Registry Contract address
     const abi = [{"inputs": [{"internalType": "bytes","name": "callData","type": "bytes"}],"name": "resolve","outputs": [{"internalType": "bytes","name": "","type": "bytes"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "_addr","type": "address"}],"name": "toString","outputs": [{"internalType": "string","name": "","type": "string"}],"stateMutability": "pure","type": "function"}];
     
     const contract = new web3.eth.Contract(abi, address);
